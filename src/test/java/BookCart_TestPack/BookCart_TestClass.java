80b1b02c5a456faed2bd9a0dacf03f3b51e6f03b
Dev <devbhagwani51@gmail.com>
2023-07-17 16:41:51 +0530

    Automate Register flow TestCase-6

--- src/test/java/BookCart_TestPack/BookCart_TestClass.java
+++ src/test/java/BookCart_TestPack/BookCart_TestClass.java
@@ -69,12 +69,31 @@ public void BookCartRegister() throws IOException, InterruptedException{
 		loginPage.clickingRegisterButton();
 		Assert.assertTrue(registrationPage.DisplayingUserRegistrationHeader());
 		registrationPage.EnteringDataInLastNameTextField();
+		registrationPage.EnteringDataInUserNameTextfield();
 		registrationPage.EnteringDataInPasswordField();
 		registrationPage.EnteringDataInConfirmPasswordField();
 		registrationPage.ClickingGenderRadioButton();
 		registrationPage.ClickingRegisterButton();
 		// First Name appears in red color
 		Thread.sleep(2000);
+	}
+	
+	@Test   // Test Case-6
+	//  Verify if user is able to register  without entering  Last Name
+	public void TestCase_6() throws InterruptedException {
+		homePage.clickingLoginButton();
+		Assert.assertTrue(loginPage.DisplayingLoginLogo());
+		loginPage.clickingRegisterButton();
+		Assert.assertTrue(registrationPage.DisplayingUserRegistrationHeader());
+		registrationPage.EnteringDataInFirstNameTextfield();
+		registrationPage.EnteringDataInUserNameTextfield();
+		registrationPage.EnteringDataInPasswordField();
+		registrationPage.EnteringDataInConfirmPasswordField();
+		registrationPage.ClickingGenderRadioButton();
+		registrationPage.ClickingRegisterButton();
+		// Last Name Appears in Red Color
+		Thread.sleep(2000);
+
 	}
 	
 	@AfterMethod
